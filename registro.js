6b90c99f009cb14c32ef205087fd5f0fc2626688
draeriel <danieljuanbarbosa@gmail.com>
2018-10-29 17:02:18 +0100

    test diario

--- registro.js
+++ registro.js
@@ -1,21 +1,38 @@
 eventos: Array;
 pulpo: Boolean;
+registros = [];
 
-
-function test() {
+function testRegistro() {
     console.log('Test registro: ');
     eventos = ["trabajar", "tocar un árbol", "hamburguesa", "correr", "twitter"];
     pulpo = false;
-    
     mostrarRegistro();
     cantidadEventos();
     contieneEvento('hamburguesa', eventos);
     contieneEvento('pizza', eventos);
 }
 
+function testDiario() {
+    console.log('Test diario: ');
+    incluirRegistro({
+        eventos: ["trabajar", "tocar un árbol", "hamburguesa", "correr", "twitter"],
+        pulpo: false
+    });
+    incluirRegistro({   
+        eventos: ["trabajar", "helado", "coliflor", "lasaña", "tocar un árbol", "lavarse los dientes"],
+        pulpo: false
+    });
+    incluirRegistro({   
+        eventos: ["finde", "bicicleta", "descansar", "cacahuetes", "cerveza"],
+        pulpo: true
+    });
+    mostrarDiario();    
+    tamañoDiario();
+}
+
 function contieneEvento(evento, eventos) {
         let containsEvent = eventos.includes(evento);
-        console.log(`${evento} ${containsEvent ? '': 'no '}está presente en el registro:`);
+        console.log(`${evento} ${containsEvent ? '' : 'no '}está presente en el registro:`);
         console.log(eventos); 
 }
 
@@ -28,5 +45,21 @@ function mostrarRegistro() {
     console.log("pulpo: " + pulpo);
 }
 
+function incluirRegistro(registro) {
+    this.registros.push(registro);
+}
 
-test();
+function mostrarDiario() {
+    for(let index = 0; index < registros.length; index++) {
+        eventos = registros[index].eventos;
+        pulpo = registros[index].pulpo;
+        mostrarRegistro();
+    }
+}
+
+function tamañoDiario() {
+    console.log(`El diario tiene ${registros.length} registros`);
+}
+
+testRegistro()
+testDiario()
