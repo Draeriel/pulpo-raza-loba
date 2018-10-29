8937530aaef948d9d7fa93a42fb4a7561d4783bc
draeriel <danieljuanbarbosa@gmail.com>
2018-10-29 16:32:01 +0100

    test 1 refactor

--- registro.js
+++ registro.js
@@ -1,20 +1,32 @@
 eventos: Array;
 pulpo: Boolean;
 
+
 function test() {
     console.log('Test registro: ');
     eventos = ["trabajar", "tocar un árbol", "hamburguesa", "correr", "twitter"];
     pulpo = false;
+    
     mostrarRegistro();
+    cantidadEventos();
+    contieneEvento('hamburguesa', eventos);
+    contieneEvento('pizza', eventos);
 }
 
 function contieneEvento(evento, eventos) {
-        console.log(eventos.includes(evento));
+        let containsEvent = eventos.includes(evento);
+        console.log(`${evento} ${containsEvent ? '': 'no '}está presente en el registro:`);
+        console.log(eventos); 
+}
+
+function cantidadEventos() {
+    console.log(`El registro tiene ${eventos.length} eventos`);
 }
 
 function mostrarRegistro() {
     console.log("eventos: " + eventos);
     console.log("pulpo: " + pulpo);
 }
+
 
 test();
